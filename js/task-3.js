8f76a68c0a1ab834076ac26fb4fb76d87668cc60
alcasinter <151970978+alcasinter@users.noreply.github.com>
2024-02-25 19:51:11 +0100

    summary

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,4 @@
+"use strict";
 function getElementWidth(content, padding, border) {
     const contentWidth = parseFloat(content);
     const paddingWidth = parseFloat(padding);
@@ -7,4 +8,4 @@ function getElementWidth(content, padding, border) {
 
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px")); 
-console.log(getElementWidth("200px", "0px";
+console.log(getElementWidth("200px", "0px"));
